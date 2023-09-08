c3b275dd55d6287241fa13adac7d1d5fd11692e7
Flagstaff49 <silvanzemp@hotmail.ch>
2023-09-08 17:04:46 +0200

    finished frontend functions and DOM manipulations

--- client/script.js
+++ client/script.js
@@ -20,7 +20,7 @@ socket.addEventListener("open", async (event) => {
   console.log("WebSocket connected!");
   // TODO: create message object to transmit the user to the backend
   const username = document.getElementById("username").value;
-  const message = { type: "user", username: username };
+  const message = { type: "newUser", username };
   socket.send(JSON.stringify(message));
 });
 
@@ -34,7 +34,7 @@ socket.addEventListener("message", (event) => {
       break;
     case "message":
       // TODO: Show new message as DOM element append to chat history
-      showMessage(messageObject.message);
+      showMessage(messageObject);
       break;
     default:
       console.error("Unknown message type: " + messageObject.type);
@@ -45,9 +45,15 @@ function showUsers(users) {
   // TODO: Show the current users as DOM elements
   const activeUsersContainer = document.getElementById("userCard");
   activeUsersContainer.innerHTML = "";
-  users.forEach((username) => {
+  users.forEach((user) => {
     const userDiv = document.createElement("div");
-    userDiv.innerHTML = "&#x1F49A " + username;
+    userDiv.className = "p-2 d-flex align-items-center mb-2 bg-light rounded";
+    const icon = document.createElement("i");
+    icon.className = "bi bi-person-fill me-2 text-success";
+    userDiv.appendChild(icon);
+    const usernameSpan = document.createElement("span");
+    usernameSpan.innerHTML = user;
+    userDiv.appendChild(usernameSpan);
     activeUsersContainer.appendChild(userDiv);
   });
 }
@@ -55,21 +61,37 @@ function showUsers(users) {
 function showMessage(message) {
   // TODO: Show new message as DOM element append to chat history
   const messageContainer = document.createElement("div");
-  const innerMessageElement = document.createElement("div");
-  const messageHeaderElement = document.createElement("span");
-  const usernameElement = document.createElement("span");
-  const timeElement = document.createElement("span");
-  const messageTextElement = document.createElement("p");
+  messageContainer.className = "mb-3";
 
+  const innerMessageElement = document.createElement("div");
+  innerMessageElement.className = "p-3 bg-light rounded";
+
+  const messageHeaderElement = document.createElement("div");
+  messageHeaderElement.className = "d-flex justify-content-between";
+
+  const usernameElement = document.createElement("span");
+  usernameElement.className = "text-primary fw-bold";
   usernameElement.innerHTML = message.username;
+
+  const timeElement = document.createElement("span");
+  timeElement.className = "text-muted";
   timeElement.innerHTML = "at " + message.time;
+
   messageHeaderElement.appendChild(usernameElement);
   messageHeaderElement.appendChild(timeElement);
+
+  const messageTextElement = document.createElement("p");
+  messageTextElement.className = "mt-2";
   messageTextElement.innerHTML = message.message;
+
   innerMessageElement.appendChild(messageHeaderElement);
   innerMessageElement.appendChild(messageTextElement);
+
   messageContainer.appendChild(innerMessageElement);
   document.getElementById("messageCard").appendChild(messageContainer);
+
+  const messageCard = document.getElementById("messageCard");
+  messageCard.scrollTop = messageCard.scrollHeight;
 }
 
 socket.addEventListener("close", (event) => {
